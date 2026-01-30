e345d8807c59a78cb483456bd9b8e8b12c2b0a79
firegate666 <marco@behnke.biz>
2026-01-30 22:39:31 +0100

    proper use of lifetime timer

--- scripts/projectile.gd
+++ scripts/projectile.gd
@@ -2,14 +2,17 @@ extends Area2D
 
 @export var speed: float = 600.0
 @export var lifetime: float = 2.0
+@export var lifetime_timer: Timer
 
 var direction: Vector2 = Vector2.ZERO
 
 func _ready():
 	body_entered.connect(_on_body_entered)
 	
-	await get_tree().create_timer(lifetime).timeout
-	on_destroy()
+	lifetime_timer.timeout.connect(_on_lifetime_timeout)
+	lifetime_timer.wait_time = lifetime
+	lifetime_timer.one_shot = true
+	lifetime_timer.start()
 
 func _physics_process(delta):
 	position += direction * speed * delta
@@ -20,6 +23,9 @@ func set_direction(dir: Vector2):
 func _on_body_entered(body):
 	if body.is_in_group("obstacle"):
 		on_destroy()
+	
+func _on_lifetime_timeout():
+	on_destroy()	
 	
 func on_destroy():
 	queue_free()
