97e3220dd58738d7226a58da038252db6054e2ea
Felix Wagner <Armag3ddon@web.de>
2026-01-31 22:46:28 +0100

    Change to single xp bar

--- scripts/ui/game_ui.gd
+++ scripts/ui/game_ui.gd
@@ -1,40 +1,18 @@
 extends Control
 
-var nature_xp: int = 0
-var water_xp: int = 0
-var fire_xp: int = 0
+var xp: int = 0
 
-@onready var nature_bar = $"2rows/3col_1/NatureXP"
-@onready var water_bar = $"2rows/3col_1/WaterXP"
-@onready var fire_bar = $"2rows/3col_1/FireXP"
+@onready var xp_bar = $"2rows/3col_1/XP"
 
 func _ready() -> void:
-	set_water_xp(0)
-	set_fire_xp(0)
-	set_nature_xp(0)
+	set_xp(0)
 
-func add_water_xp(number: int) -> void:
-	set_water_xp(water_xp + number)
+func add_xp(number: int) -> void:
+	set_xp(xp + number)
 
-func set_water_xp(number: int) -> void:
-	water_xp = number
-	update_bars()
+func set_xp(number: int) -> void:
+	xp = number
+	update_bar()
 
-func add_fire_xp(number: int) -> void:
-	set_fire_xp(fire_xp + number)
-
-func set_fire_xp(number: int) -> void:
-	fire_xp = number
-	update_bars()
-
-func add_nature_xp(number: int) -> void:
-	set_nature_xp(nature_xp + number)
-
-func set_nature_xp(number: int) -> void:
-	nature_xp = number
-	update_bars()
-
-func update_bars() -> void:
-	nature_bar.value = nature_xp
-	water_bar.value = water_xp
-	fire_bar.value = fire_xp
+func update_bar() -> void:
+	xp_bar.value = xp
