cc2465e08c806d4bb2cadc9f56d87f7f023204dd
Tobias Rojahn <tobias.rojahn@gmail.com>
2026-01-31 14:53:42 +0100

    enemy spaning

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,19 +1,30 @@
 extends Node2D
 
-var spawn_timer: float = 10000.0
+var spawn_distance: float = 2500.0
+var spawn_timer: float = 0.0
 var quit_dialog: ConfirmationDialog
+
+var current_wave = 0
+var waves = []
 
 func _ready() -> void:
 	init_quit_game_dialog()
+	
+	var file = FileAccess.open("data/waves.json", FileAccess.READ)
+	waves = JSON.parse_string(file.get_as_text())
 
 func _process(delta: float) -> void:
 	spawn_timer = spawn_timer + delta
-	if spawn_timer > 10000.0:
-		var random_angle: float = randf() * 360.0
-		var enemy_pos := Vector2(cos(deg_to_rad(random_angle))*1500.0, sin(deg_to_rad(random_angle))*1500.0)
-		var enemy := Enemy.spawn(enemy_pos, $Player)
+	if current_wave < len(waves) && spawn_timer > waves[current_wave].delay:
+		spawn_wave(waves[current_wave])
+		current_wave += 1
+		
+func spawn_wave(wave):
+	for n in wave.enemies:
+		var random_angle: float = deg_to_rad(randf() * 360.0)
+		var enemy_pos := Vector2(cos(random_angle)*spawn_distance, sin(random_angle)*spawn_distance)
+		var enemy := Enemy.spawn($Player.position + enemy_pos, $Player)
 		add_child(enemy)
-		spawn_timer = 0.0
 
 func _input(event):
 	if event.is_action_pressed("exit_game"):
