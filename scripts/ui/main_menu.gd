4f0d55821dee04e220446ca5ba9ac5401b8575cb
Felix Wagner <Armag3ddon@web.de>
2026-02-01 17:04:26 +0100

    Fix lag

--- scripts/ui/main_menu.gd
+++ scripts/ui/main_menu.gd
@@ -12,3 +12,8 @@ func _on_exit_pressed() -> void:
 
 func _on_credits_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/credits.tscn")
+
+
+func _ready() -> void:
+	var effect = DeathEffect.spawn(Vector2(-100.0, -100.0))
+	add_child(effect)
