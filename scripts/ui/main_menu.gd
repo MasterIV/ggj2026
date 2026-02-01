02247051f39ef146a7535279d6b899296bc40bd2
Felix Wagner <Armag3ddon@web.de>
2026-02-01 15:10:03 +0100

    create credits screen

--- scripts/ui/main_menu.gd
+++ scripts/ui/main_menu.gd
@@ -8,3 +8,7 @@ func _on_new_game_pressed() -> void:
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_credits_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/credits.tscn")
