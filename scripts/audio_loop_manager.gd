a450207b57d9e58728717aa07b6c9790a35afc37
firegate666 <marco@behnke.biz>
2026-02-02 16:01:00 +0100

    use seek and play/stop instead of volume manipulation

--- scripts/audio_loop_manager.gd
+++ scripts/audio_loop_manager.gd
@@ -9,7 +9,6 @@ extends Node
 signal mask_changed(mask_type: Enums.Element)
 
 func _ready() -> void:
-	print("Start audio looper")
 	standard_loop.play()
 	aqua_loop.play()
 	fire_loop.play()
@@ -20,9 +19,14 @@ func _ready() -> void:
 	_on_mask_changed(Enums.Element.AQUA)
 
 func _on_mask_changed(mask_type: Enums.Element):
-	AudioServer.set_bus_volume_db(get_audio_bus_index("Aqua"), -6.0 if mask_type == Enums.Element.AQUA else -80.0)
-	AudioServer.set_bus_volume_db(get_audio_bus_index("Fire"), -6.0 if mask_type == Enums.Element.FIRE else -80.0)
-	AudioServer.set_bus_volume_db(get_audio_bus_index("Nature"), -6.0 if mask_type == Enums.Element.NATURE else -80.0)
+	fire_loop.stop()
+	aqua_loop.stop()
+	nature_loop.stop()
 
-func get_audio_bus_index(audio_bus_name: String):
-	return AudioServer.get_bus_index(audio_bus_name)
+	match mask_type:
+		Enums.Element.AQUA:
+			aqua_loop.play(standard_loop.get_playback_position())
+		Enums.Element.FIRE:
+			fire_loop.play(standard_loop.get_playback_position())
+		Enums.Element.NATURE:
+			nature_loop.play(standard_loop.get_playback_position())
