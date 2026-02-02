aac85013fab6ed1143c258b1b108ed0fd532a9c0
firegate666 <marco@behnke.biz>
2026-02-02 15:55:10 +0100

    replace audio 2d wiuth audio to avoid losing volume

--- scripts/audio_loop_manager.gd
+++ scripts/audio_loop_manager.gd
@@ -1,10 +1,10 @@
 class_name AudioLoopManager
 extends Node
 
-@export var standard_loop: AudioStreamPlayer2D
-@export var aqua_loop: AudioStreamPlayer2D
-@export var fire_loop: AudioStreamPlayer2D
-@export var nature_loop: AudioStreamPlayer2D
+@export var standard_loop: AudioStreamPlayer
+@export var aqua_loop: AudioStreamPlayer
+@export var fire_loop: AudioStreamPlayer
+@export var nature_loop: AudioStreamPlayer
 
 signal mask_changed(mask_type: Enums.Element)
 
@@ -20,9 +20,9 @@ func _ready() -> void:
 	_on_mask_changed(Enums.Element.AQUA)
 
 func _on_mask_changed(mask_type: Enums.Element):
-	AudioServer.set_bus_volume_db(get_audio_bus_index("Aqua"), 0.0 if mask_type == Enums.Element.AQUA else -80.0)
-	AudioServer.set_bus_volume_db(get_audio_bus_index("Fire"), 0.0 if mask_type == Enums.Element.FIRE else -80.0)
-	AudioServer.set_bus_volume_db(get_audio_bus_index("Nature"), 0.0 if mask_type == Enums.Element.NATURE else -80.0)
+	AudioServer.set_bus_volume_db(get_audio_bus_index("Aqua"), -6.0 if mask_type == Enums.Element.AQUA else -80.0)
+	AudioServer.set_bus_volume_db(get_audio_bus_index("Fire"), -6.0 if mask_type == Enums.Element.FIRE else -80.0)
+	AudioServer.set_bus_volume_db(get_audio_bus_index("Nature"), -6.0 if mask_type == Enums.Element.NATURE else -80.0)
 
 func get_audio_bus_index(audio_bus_name: String):
 	return AudioServer.get_bus_index(audio_bus_name)
