b63fb60035f311e045148556a42027825f76b3cf
firegate666 <marco@behnke.biz>
2026-02-03 00:01:43 +0100

    add F11 fullscreen toggle and hide exit in web

--- scripts/ui/credits.gd
+++ scripts/ui/credits.gd
@@ -3,3 +3,10 @@ extends Node2D
 
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/ui/main_menu.tscn")
+
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("toggle_fullscreen"):
+		if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+		else:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
--- scripts/ui/main_menu.gd
+++ scripts/ui/main_menu.gd
@@ -1,19 +1,27 @@
 extends Node2D
 
+@export var web_incompatible: Array[Node]
 
 func _on_new_game_pressed() -> void:
 	get_tree().change_scene_to_file("res://main.tscn")
 
-
-
 func _on_exit_pressed() -> void:
 	get_tree().quit()
-
 
 func _on_credits_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/ui/credits.tscn")
 
-
 func _ready() -> void:
 	var effect = DeathEffect.spawn(Vector2(-100.0, -100.0))
 	add_child(effect)
+	
+	if OS.has_feature("web"):
+		for item in web_incompatible:
+			item.hide()
+
+func _process(_delta):
+	if Input.is_action_just_pressed("toggle_fullscreen"):
+		if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+		else:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
